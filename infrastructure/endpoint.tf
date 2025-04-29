c58032e7293c89ac48407a05fd8ff3dfd1dcfa83
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-04-29 09:48:47 -0700

    endpoints can't have static IPs

--- infrastructure/endpoint.tf
+++ infrastructure/endpoint.tf
@@ -3,10 +3,10 @@ resource "aws_vpc_endpoint" "ec2_service_vpce" {
   service_name      = "com.amazonaws.us-west-2.ec2"
   vpc_endpoint_type = "Interface"
 
-  subnet_configuration {
-    ipv4      = "192.168.2.31"
-    subnet_id = aws_subnet.vpce_subnet.id
-  }
+#   subnet_configuration {
+#     ipv4      = "192.168.2.31"
+#     subnet_id = aws_subnet.vpce_subnet.id
+#   }
 
   subnet_ids = [aws_subnet.vpce_subnet.id]
 
@@ -23,10 +23,10 @@ resource "aws_vpc_endpoint" "thirdparty_vpce" {
   service_name      = "com.amazonaws.us-west-2.s3"
   vpc_endpoint_type = "Interface"
 
-  subnet_configuration {
-    ipv4      = "10.69.0.32"
-    subnet_id = aws_subnet.thirdparty_subnet.id
-  }
+#   subnet_configuration {
+#     ipv4      = "10.69.0.32"
+#     subnet_id = aws_subnet.thirdparty_subnet.id
+#   }
 
   subnet_ids = [aws_subnet.thirdparty_subnet.id]
 
