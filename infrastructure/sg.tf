df9162fefd8592c730afa94170fd6701423d6145
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-04-28 21:52:00 -0700

    first infra commit

---
+++ infrastructure/sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Empty SG for EC2"
+  vpc_id      = aws_vpc.test_vpc.id
+}
+
+resource "aws_security_group" "vpc_endpoint_sg" {
+  name        = "vpce-sg"
+  description = "Empty SG for VPC endpoint"
+  vpc_id      = aws_vpc.test_vpc.id
+}
+
+resource "aws_security_group" "thirdparty_vpce_sg" {
+  name        = "thirdparty-vpce-sg"
+  description = "Empty SG for Thirdparty PrivateLink endpoint"
+  vpc_id      = aws_vpc.test_vpc.id
+}
+
+resource "aws_security_group" "paloalto_vm_sg" {
+  name        = "thirdparty-vpce-sg"
+  description = "Palo Alto VM"
+  vpc_id      = aws_vpc.test_vpc.id
+}
