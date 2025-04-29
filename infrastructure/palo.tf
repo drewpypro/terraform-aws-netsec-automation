ee0b555daf8a380177b37924ff5a1297917c4725
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-04-29 10:07:45 -0700

    updated cidr

--- infrastructure/palo.tf
+++ infrastructure/palo.tf
@@ -21,12 +21,20 @@ resource "aws_network_interface" "palo_mgmt" {
   subnet_id       = aws_subnet.ec2_subnet.id
   private_ips     = ["192.168.1.100"]
   security_groups = [aws_security_group.paloalto_vm_sg.id]
+
+  tags = {
+    Name = "palo-mgmt-eni"
+  }
 }
 
 resource "aws_network_interface" "palo_dataplane" {
   subnet_id       = aws_subnet.firewall_subnet.id
   private_ips     = ["192.168.3.20"]
   security_groups = [aws_security_group.paloalto_vm_sg.id]
+
+  tags = {
+    Name = "palo-dataplane-eni"
+  }
 }
 
 
