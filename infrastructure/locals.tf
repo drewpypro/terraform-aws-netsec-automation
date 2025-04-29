0b2e59c331fa0de5559a4a400bf10b19969ddfd6
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-04-29 14:39:09 -0700

    update locals

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -38,8 +38,8 @@ locals {
     palo_rules = [
     for i, req in local.raw.requests : {
         name              = "${local.raw.request_id}-${req.protocol}-${req.port}"
-        source_ip         = req.source.ips[0]
-        destination_ip    = req.destination.ips[0]
+        source_ip = can(req.source.ips[0]) ? req.source.ips[0] : "any"
+        destination_ip    = can(req.destination.ips[0]) ? req.destination.ips[0] : "any"
         protocol          = req.protocol
         port              = req.port
         appid             = req.appid
