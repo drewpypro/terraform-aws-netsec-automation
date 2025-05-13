78b918e6032a06178d43c11d10c68c516c645f24
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-05-13 11:23:21 -0700

    testing multiregion

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,7 @@ module "us_west_2" {
   source         = "./modules/network_stack"
   region         = "us-west-2"
   policies_path  = "${path.module}/policies/us-west-2"
+  vpc_cidr_block = var.vpc_cidr_block["us-west-2"]
   providers = {
     aws   = aws.us-west-2
     panos = panos.us-west-2
@@ -12,6 +13,7 @@ module "us_east_1" {
   source         = "./modules/network_stack"
   region         = "us-east-1"
   policies_path  = "${path.module}/policies/us-east-1"
+  vpc_cidr_block = var.vpc_cidr_block["us-east-1"]
   providers = {
     aws   = aws.us-east-1
     panos = panos.us-east-1
