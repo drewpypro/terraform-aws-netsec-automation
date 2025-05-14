37a3cd0599208175e934f34aca62eaea222544d2
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-05-14 07:52:24 -0700

    multi-region refactor

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,17 @@
 variable "panos_hostname" {
   description = "Hostname of the Palo Alto firewall or Panorama"
   type        = string
+  default     = "test-123"
 }
 
 variable "panos_username" {
   description = "Username for the Palo Alto firewall or Panorama"
   type        = string
+  default     = "test-123"
 }
 
 variable "panos_password" {
   description = "Password for the Palo Alto firewall or Panorama"
   type        = string
-  sensitive   = true
+  default     = "test-123"
 }
