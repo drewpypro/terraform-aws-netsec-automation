fcec33da87afeb3566073746f62d9984aed3b4e5
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-06-06 14:45:09 -0700

    build again

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,147 +1,147 @@
-# # Create consumer security groups for us-east-1 region
-# module "consumer_sg_us_east_1" {
-#   source = "./modules/consumer"
+# Create consumer security groups for us-east-1 region
+module "consumer_sg_us_east_1" {
+  source = "./modules/consumer"
   
-#   for_each = try(local.consumer_sg_rules["us-east-1"], {})
+  for_each = try(local.consumer_sg_rules["us-east-1"], {})
   
-#   providers = { 
-#     aws = aws.us_east_1
-#     # panos = panos
-#   }
+  providers = { 
+    aws = aws.us_east_1
+    # panos = panos
+  }
   
-#   # Take values from the first rule in the group
-#   # Security group settings
-#   region = "us-east-1"
-#   security_group_name = each.key  # This is the sg_name
-#   security_group_description = each.value[0].sg_description
-#   vpc_id = each.value[0].policy.security_group.vpc_id
-#   tags = each.value[0].tags
+  # Take values from the first rule in the group
+  # Security group settings
+  region = "us-east-1"
+  security_group_name = each.key  # This is the sg_name
+  security_group_description = each.value[0].sg_description
+  vpc_id = each.value[0].policy.security_group.vpc_id
+  tags = each.value[0].tags
   
-#   # Take the first rule's details
-#   protocol = each.value[0].rule.protocol
-#   from_port = each.value[0].rule.port
-#   to_port = each.value[0].rule.port
-#   source_cidrs = [for rule in each.value : rule.cidr]
-#   destination_cidrs = [""]
-#   description = "Allow access from ${each.value[0].rule.source.account_id} (${each.value[0].rule.request_id})"
-#   rule_tags = each.value[0].rule_tags
+  # Take the first rule's details
+  protocol = each.value[0].rule.protocol
+  from_port = each.value[0].rule.port
+  to_port = each.value[0].rule.port
+  source_cidrs = [for rule in each.value : rule.cidr]
+  destination_cidrs = [""]
+  description = "Allow access from ${each.value[0].rule.source.account_id} (${each.value[0].rule.request_id})"
+  rule_tags = each.value[0].rule_tags
   
-#   # Palo Alto settings
-#   enable_palo_inspection = false
-#   name_prefix = each.value[0].policy.security_group.thirdpartyName
-#   request_id = each.value[0].rule.request_id
-#   appid = each.value[0].rule.appid
-#   url = each.value[0].rule.url
+  # Palo Alto settings
+  enable_palo_inspection = false
+  name_prefix = each.value[0].policy.security_group.thirdpartyName
+  request_id = each.value[0].rule.request_id
+  appid = each.value[0].rule.appid
+  url = each.value[0].rule.url
   
-# }
+}
 
-# # Create provider security groups for us-east-1 region
-# module "provider_sg_us_east_1" {
-#   source = "./modules/provider"
+# Create provider security groups for us-east-1 region
+module "provider_sg_us_east_1" {
+  source = "./modules/provider"
   
-#   for_each = try(local.provider_sg_rules["us-east-1"], {})
+  for_each = try(local.provider_sg_rules["us-east-1"], {})
   
-#   providers = { 
-#     aws = aws.us_east_1
-#     # panos = panos
-#   }
+  providers = { 
+    aws = aws.us_east_1
+    # panos = panos
+  }
   
-#   # Take values from the first rule in the group
-#   # Security group settings
-#   region = "us-east-1"
-#   security_group_name = each.key  # This is the sg_name
-#   security_group_description = each.value[0].sg_description
-#   vpc_id = each.value[0].policy.security_group.vpc_id
-#   tags = each.value[0].tags
+  # Take values from the first rule in the group
+  # Security group settings
+  region = "us-east-1"
+  security_group_name = each.key  # This is the sg_name
+  security_group_description = each.value[0].sg_description
+  vpc_id = each.value[0].policy.security_group.vpc_id
+  tags = each.value[0].tags
   
-#   # Take the first rule's details
-#   protocol = each.value[0].rule.protocol
-#   from_port = each.value[0].rule.port
-#   to_port = each.value[0].rule.port
-#   destination_cidrs = [for rule in each.value : rule.cidr]
-#   source_cidrs = [""]
-#   description = "Allow access to backend (${each.value[0].rule.request_id})"
-#   rule_tags = each.value[0].rule_tags
+  # Take the first rule's details
+  protocol = each.value[0].rule.protocol
+  from_port = each.value[0].rule.port
+  to_port = each.value[0].rule.port
+  destination_cidrs = [for rule in each.value : rule.cidr]
+  source_cidrs = [""]
+  description = "Allow access to backend (${each.value[0].rule.request_id})"
+  rule_tags = each.value[0].rule_tags
   
-#   # Palo Alto settings
-#   enable_palo_inspection = false
-#   name_prefix = each.value[0].policy.security_group.internalAppID
-#   request_id = each.value[0].rule.request_id
-#   appid = each.value[0].rule.appid
-#   url = each.value[0].rule.url
+  # Palo Alto settings
+  enable_palo_inspection = false
+  name_prefix = each.value[0].policy.security_group.internalAppID
+  request_id = each.value[0].rule.request_id
+  appid = each.value[0].rule.appid
+  url = each.value[0].rule.url
 
-# }
+}
 
-# # Create consumer security groups for us-west-2 region
-# module "consumer_sg_us_west_2" {
-#   source = "./modules/consumer"
+# Create consumer security groups for us-west-2 region
+module "consumer_sg_us_west_2" {
+  source = "./modules/consumer"
   
-#   for_each = try(local.consumer_sg_rules["us-west-2"], {})
+  for_each = try(local.consumer_sg_rules["us-west-2"], {})
   
-#   providers = { 
-#     aws = aws.us_west_2
-#     # panos = panos
-#   }
+  providers = { 
+    aws = aws.us_west_2
+    # panos = panos
+  }
   
-#   # Take values from the first rule in the group
-#   # Security group settings
-#   region = "us-west-2"
-#   security_group_name = each.key  # This is the sg_name
-#   security_group_description = each.value[0].sg_description
-#   vpc_id = each.value[0].policy.security_group.vpc_id
-#   tags = each.value[0].tags
+  # Take values from the first rule in the group
+  # Security group settings
+  region = "us-west-2"
+  security_group_name = each.key  # This is the sg_name
+  security_group_description = each.value[0].sg_description
+  vpc_id = each.value[0].policy.security_group.vpc_id
+  tags = each.value[0].tags
   
-#   # Take the first rule's details
-#   protocol = each.value[0].rule.protocol
-#   from_port = each.value[0].rule.port
-#   to_port = each.value[0].rule.port
-#   source_cidrs = [for rule in each.value : rule.cidr]
-#   destination_cidrs = [""]
-#   description = "Allow access from ${each.value[0].rule.source.account_id} (${each.value[0].rule.request_id})"
-#   rule_tags = each.value[0].rule_tags
+  # Take the first rule's details
+  protocol = each.value[0].rule.protocol
+  from_port = each.value[0].rule.port
+  to_port = each.value[0].rule.port
+  source_cidrs = [for rule in each.value : rule.cidr]
+  destination_cidrs = [""]
+  description = "Allow access from ${each.value[0].rule.source.account_id} (${each.value[0].rule.request_id})"
+  rule_tags = each.value[0].rule_tags
   
-#   # Palo Alto settings
-#   enable_palo_inspection = false
-#   name_prefix = each.value[0].policy.security_group.thirdpartyName
-#   request_id = each.value[0].rule.request_id
-#   appid = each.value[0].rule.appid
-#   url = each.value[0].rule.url
+  # Palo Alto settings
+  enable_palo_inspection = false
+  name_prefix = each.value[0].policy.security_group.thirdpartyName
+  request_id = each.value[0].rule.request_id
+  appid = each.value[0].rule.appid
+  url = each.value[0].rule.url
   
-# }
+}
 
-# # Create provider security groups for us-west-2 region
-# module "provider_sg_us_west_2" {
-#   source = "./modules/provider"
+# Create provider security groups for us-west-2 region
+module "provider_sg_us_west_2" {
+  source = "./modules/provider"
   
-#   for_each = try(local.provider_sg_rules["us-west-2"], {})
+  for_each = try(local.provider_sg_rules["us-west-2"], {})
   
-#   providers = { 
-#     aws = aws.us_west_2
-#     # panos = panos
-#   }
+  providers = { 
+    aws = aws.us_west_2
+    # panos = panos
+  }
   
-#   # Take values from the first rule in the group
-#   # Security group settings
-#   region = "us-west-2"
-#   security_group_name = each.key  # This is the sg_name
-#   security_group_description = each.value[0].sg_description
-#   vpc_id = each.value[0].policy.security_group.vpc_id
-#   tags = each.value[0].tags
+  # Take values from the first rule in the group
+  # Security group settings
+  region = "us-west-2"
+  security_group_name = each.key  # This is the sg_name
+  security_group_description = each.value[0].sg_description
+  vpc_id = each.value[0].policy.security_group.vpc_id
+  tags = each.value[0].tags
   
-#   # Take the first rule's details
-#   protocol = each.value[0].rule.protocol
-#   from_port = each.value[0].rule.port
-#   to_port = each.value[0].rule.port
-#   destination_cidrs = [for rule in each.value : rule.cidr]
-#   source_cidrs = [""]
-#   description = "Allow access to backend (${each.value[0].rule.request_id})"
-#   rule_tags = each.value[0].rule_tags
+  # Take the first rule's details
+  protocol = each.value[0].rule.protocol
+  from_port = each.value[0].rule.port
+  to_port = each.value[0].rule.port
+  destination_cidrs = [for rule in each.value : rule.cidr]
+  source_cidrs = [""]
+  description = "Allow access to backend (${each.value[0].rule.request_id})"
+  rule_tags = each.value[0].rule_tags
   
-#   # Palo Alto settings
-#   enable_palo_inspection = false
-#   name_prefix = each.value[0].policy.security_group.internalAppID
-#   request_id = each.value[0].rule.request_id
-#   appid = each.value[0].rule.appid
-#   url = each.value[0].rule.url
+  # Palo Alto settings
+  enable_palo_inspection = false
+  name_prefix = each.value[0].policy.security_group.internalAppID
+  request_id = each.value[0].rule.request_id
+  appid = each.value[0].rule.appid
+  url = each.value[0].rule.url
   
-# }
+}
--- infrastructure/modules/consumer/palo.tf
+++ infrastructure/modules/consumer/palo.tf
@@ -1,26 +1,26 @@
-# # Create Panorama rule for consumer (ingress) traffic in the appropriate device group
-# resource "panos_panorama_security_rule_group" "rule" {
-#   count = var.enable_palo_inspection ? 1 : 0
+# Create Panorama rule for consumer (ingress) traffic in the appropriate device group
+resource "panos_panorama_security_rule_group" "rule" {
+  count = var.enable_palo_inspection ? 1 : 0
   
-#   device_group = "${var.region}-fw-dg"  # Regional device group
-#   position_keyword = "bottom"
+  device_group = "${var.region}-fw-dg"  # Regional device group
+  position_keyword = "bottom"
   
-#   rule {
-#     name                  = "pl-consumer-${var.name_prefix}-${var.request_id}"
-#     source_zones          = ["any"]
-#     source_addresses      = var.source_cidrs
-#     source_users          = ["any"]
-#     destination_zones     = ["any"]
-#     destination_addresses = ["100.64.0.0/24"]  # Privatelink Endpoint Subnet
-#     applications          = [var.appid]
-#     services              = ["application-default"]
-#     categories            = ["any"]
-#     action                = "allow"
-#     description           = "${var.description} (${var.request_id})"
+  rule {
+    name                  = "pl-consumer-${var.name_prefix}-${var.request_id}"
+    source_zones          = ["any"]
+    source_addresses      = var.source_cidrs
+    source_users          = ["any"]
+    destination_zones     = ["any"]
+    destination_addresses = ["100.64.0.0/24"]  # Privatelink Endpoint Subnet
+    applications          = [var.appid]
+    services              = ["application-default"]
+    categories            = ["any"]
+    action                = "allow"
+    description           = "${var.description} (${var.request_id})"
     
-#     tags = [
-#       "managed-by-terraform",
-#       "privatelink-consumer",
-#     ]
-#   }
-# }
+    tags = [
+      "managed-by-terraform",
+      "privatelink-consumer",
+    ]
+  }
+}
