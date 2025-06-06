aa33604a7204151062b81624e26507d668330da6
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-06-06 14:51:58 -0700

    build again

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # panos = {
-    #     source  = "paloaltonetworks/panos"
-    #     version = "~> 1.11.0"
-    # }
     }
   backend "s3" {
     skip_credentials_validation = true
