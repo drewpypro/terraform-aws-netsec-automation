53c7c231137e01467eb4a35cf9a38f7af248b9c1
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-08-05 09:32:42 -0700

    create global pan obj group

--- infrastructure/modules/palo-objects/main.tf
+++ infrastructure/modules/palo-objects/main.tf
@@ -1,7 +1,7 @@
 resource "panos_panorama_service_object" "service_objs" {
   for_each         = toset(var.services)
 
-  device_group     = "${var.region}-fw-dg"
+  device_group     = "AWS"
   name             = each.value
   protocol         = split("-", each.value)[0]
   destination_port = split("-", each.value)[1]
@@ -11,16 +11,16 @@ resource "panos_panorama_service_object" "service_objs" {
 resource "panos_panorama_administrative_tag" "tag_objs" {
   for_each     = toset(var.tags)
 
-  device_group = "${var.region}-fw-dg"
+  device_group = "AWS"
   name         = each.value
   color        = "color6"
   comment      = "Auto-tag for rule ${each.value}"
 }
 
-resource "panos_panorama_custom_url_category" "url_objs" {
+resource "panos_custom_url_category" "url_objs" {
   for_each     = toset(var.urls)
 
-  device_group = "${var.region}-fw-dg"
+  device_group = "AWS"
   name         = replace(replace(replace(each.value, "https://", ""), ".", "-"), "/", "-")
   description  = each.value
   type         = "URL List"
