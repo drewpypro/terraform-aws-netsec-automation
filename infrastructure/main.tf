6bf925839dc6bab6b0099b5fad0af27f1f490278
drewpypro <83561525+drewpypro@users.noreply.github.com>
2025-08-04 20:30:28 -0700

    added modules

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,68 +1,68 @@
-# locals {
-#   policy_files_v2 = fileset("${path.module}/policies_v2", "*-policy.json")
+locals {
+  policy_files_v2 = fileset("${path.module}/policies_v2", "*-policy.json")
 
-#   policies_v2 = {
-#     for file in local.policy_files_v2 :
-#     trimsuffix(file, "-policy.json") => jsondecode(file("${path.module}/policies_v2/${file}"))
-#   }
+  policies_v2 = {
+    for file in local.policy_files_v2 :
+    trimsuffix(file, "-policy.json") => jsondecode(file("${path.module}/policies_v2/${file}"))
+  }
 
-#   grouped_policies_v2 = {
-#     for key, policy_v2 in local.policies_v2 :
-#     "${policy_v2.tags.Thirdparty}-${policy_v2.tags.ServiceType}-${policy_v2.region}" => policy_v2
-#   }
-# }
+  grouped_policies_v2 = {
+    for key, policy_v2 in local.policies_v2 :
+    "${policy_v2.tags.Thirdparty}-${policy_v2.tags.ServiceType}-${policy_v2.region}" => policy_v2
+  }
+}
 
 
-# module "consumer_us_east_1_v2" {
-#   source = "./modules/consumer"
-#   providers = { 
-#     aws = aws.us_east_1
-#     panos = panos
-#   }
-#   for_each = local.grouped_policies_v2
+module "consumer_us_east_1_v2" {
+  source = "./modules/consumer"
+  providers = { 
+    aws = aws.us_east_1
+    panos = panos
+  }
+  for_each = local.grouped_policies_v2
 
-#   sg_name     = each.value.security_group_name
-#   vpc_id      = module.vpc_us_east_1.vpc_id
-#   region      = each.value.region
-#   tags        = each.value.tags
-#   aws_rules   = each.value.aws_rules
-#   palo_rules  = each.value.palo_rules
+  sg_name     = each.value.security_group_name
+  vpc_id      = module.vpc_us_east_1.vpc_id
+  region      = each.value.region
+  tags        = each.value.tags
+  aws_rules   = each.value.aws_rules
+  palo_rules  = each.value.palo_rules
 
-#   depends_on = [module.vpc_us_east_1]
-# }
+  depends_on = [module.vpc_us_east_1]
+}
 
-# # Create consumer security groups for us-east-1 region
-# module "consumer_sg_us_east_1_v1" {
-#   source = "./modules/consumer-old/"
+# Create consumer security groups for us-east-1 region
+module "consumer_sg_us_east_1_v1" {
+  source = "./modules/consumer-old/"
   
-#   for_each = try(local.consumer_sgs_by_region["us-east-1"], {})
+  for_each = try(local.consumer_sgs_by_region["us-east-1"], {})
   
-#   providers = { 
-#     aws = aws.us_east_1
-#     panos = panos
-#   }
+  providers = { 
+    aws = aws.us_east_1
+    panos = panos
+  }
   
-#   # Security group settings
-#   region = "us-east-1"
-#   security_group_name = each.value.sg_name
-#   security_group_description = each.value.sg_description
-#   vpc_id = each.value.vpc_id
-#   tags = each.value.tags
-#   service_name = each.value.tags.ServiceName
+  # Security group settings
+  region = "us-east-1"
+  security_group_name = each.value.sg_name
+  security_group_description = each.value.sg_description
+  vpc_id = each.value.vpc_id
+  tags = each.value.tags
+  service_name = each.value.tags.ServiceName
   
-#   # AWS security group rules (pre-processed, no loops needed in module)
-#   aws_rules = each.value.aws_rules
+  # AWS security group rules (pre-processed, no loops needed in module)
+  aws_rules = each.value.aws_rules
   
-#   # Palo Alto settings  
-#   enable_palo_inspection = each.value.enable_palo_inspection
-#   name_prefix = each.value.name_prefix
-#   request_id = each.value.request_id
-#   appid = each.value.appid
-#   url = each.value.url
-#   palo_protocols_ports = each.value.palo_protocols_ports
-#   palo_source_ips = each.value.palo_source_ips
-#   palo_rules = each.value.palo_rules
+  # Palo Alto settings  
+  enable_palo_inspection = each.value.enable_palo_inspection
+  name_prefix = each.value.name_prefix
+  request_id = each.value.request_id
+  appid = each.value.appid
+  url = each.value.url
+  palo_protocols_ports = each.value.palo_protocols_ports
+  palo_source_ips = each.value.palo_source_ips
+  palo_rules = each.value.palo_rules
 
-#   depends_on = [module.vpc_us_east_1]
-# }
+  depends_on = [module.vpc_us_east_1]
+}
 
--- infrastructure/modules/consumer/palo.tf
+++ infrastructure/modules/consumer/palo.tf
@@ -1,49 +1,49 @@
 
-# resource "panos_panorama_service_object" "consumer_services" {
-#   count            = length(var.palo_rules)
-#   name             = "svc-${count.index}"
-#   device_group     = "${var.region}-fw-dg"
-#   protocol         = var.palo_rules[count.index].protocol
-#   destination_port = var.palo_rules[count.index].port
-# }
+resource "panos_panorama_service_object" "consumer_services" {
+  count            = length(var.palo_rules)
+  name             = "svc-${count.index}"
+  device_group     = "${var.region}-fw-dg"
+  protocol         = var.palo_rules[count.index].protocol
+  destination_port = var.palo_rules[count.index].port
+}
 
-# # resource "panos_panorama_administrative_tag" "consumer_tag" {
-# #   count        = length(var.palo_rules)
-# #   name         = "tag-${count.index}"
-# #   device_group = "${var.region}-fw-dg"
-# #   color        = "color6"
-# #   comment      = "Auto-tag for rule ${count.index}"
-# # }
-
-
-# resource "panos_custom_url_category" "consumer_category" {
+# resource "panos_panorama_administrative_tag" "consumer_tag" {
 #   count        = length(var.palo_rules)
-#   name         = substr(replace(replace(var.palo_rules[count.index].url, "https://", ""), "/", "-"), 0, 63)
+#   name         = "tag-${count.index}"
 #   device_group = "${var.region}-fw-dg"
-#   sites        = [replace(var.palo_rules[count.index].url, "https://", "")]
-#   type         = "URL List"
+#   color        = "color6"
+#   comment      = "Auto-tag for rule ${count.index}"
 # }
 
 
-# resource "panos_panorama_security_rule_group" "consumer_group" {
-#   for_each     = { for idx, rule in var.palo_rules : idx => rule }
-#   rulebase     = "pre-rulebase"
-#   device_group = "${var.region}-fw-dg"
+resource "panos_custom_url_category" "consumer_category" {
+  count        = length(var.palo_rules)
+  name         = substr(replace(replace(var.palo_rules[count.index].url, "https://", ""), "/", "-"), 0, 63)
+  device_group = "${var.region}-fw-dg"
+  sites        = [replace(var.palo_rules[count.index].url, "https://", "")]
+  type         = "URL List"
+}
 
-#   rule {
-#     name                  = "rule-${each.key}"
-#     applications           = [each.value.appid]
-#     source_zones          = ["trust"]
-#     destination_zones     = ["untrust"]
-#     source_addresses      = each.value.source_ips
-#     destination_addresses = ["any"]
-#     services               = [panos_panorama_service_object.consumer_services[each.key].name]
-#     action                = "allow"
-#     categories              = [panos_custom_url_category.consumer_category[each.key].name]
-#     source_users           = ["any"]
-#     tags = [
-#       "managed-by-terraform",
-#       "privatelink-consumer",
-#     ]
-#   }
-# }
+
+resource "panos_panorama_security_rule_group" "consumer_group" {
+  for_each     = { for idx, rule in var.palo_rules : idx => rule }
+  rulebase     = "pre-rulebase"
+  device_group = "${var.region}-fw-dg"
+
+  rule {
+    name                  = "rule-${each.key}"
+    applications           = [each.value.appid]
+    source_zones          = ["trust"]
+    destination_zones     = ["untrust"]
+    source_addresses      = each.value.source_ips
+    destination_addresses = ["any"]
+    services               = [panos_panorama_service_object.consumer_services[each.key].name]
+    action                = "allow"
+    categories              = [panos_custom_url_category.consumer_category[each.key].name]
+    source_users           = ["any"]
+    tags = [
+      "managed-by-terraform",
+      "privatelink-consumer",
+    ]
+  }
+}
